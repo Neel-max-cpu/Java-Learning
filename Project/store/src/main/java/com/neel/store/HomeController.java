3cfd8a22031eeed889e9ceb1e62b670f288b90d0
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-04-22 17:46:45 +0530

    doing :memo:

--- Project/store/src/main/java/com/neel/store/HomeController.java
+++ Project/store/src/main/java/com/neel/store/HomeController.java
@@ -1,13 +1,21 @@
 package com.neel.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String x;
+
+
     // api calling in "/"
     @RequestMapping("/")
     public String index(){
+        System.out.println("appName: "+x);
+        System.out.println("appName: yo");
+        System.out.println("appName: hehe");
         return "index.html";
     }
 }
